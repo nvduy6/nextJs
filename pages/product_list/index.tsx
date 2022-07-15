2146520e18202707818570427858e08a51d2f2c3
duynvph15542 <duynvph15542@fpt.edu.vn>
2022-07-15 20:43:27 +0700

    add_product

---
+++ pages/product_list/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import LayoutAdmin from '../../component/layout/admin'
+import AdminPage from '../admin'
+
+type Props = {}
+
+const List_product = (props: Props) => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+List_product.Layout=LayoutAdmin
+export default List_product
